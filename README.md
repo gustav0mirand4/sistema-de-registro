dc9194edf8e78e9965598df46ea1a82d45fe7d9b
ACII-TI <ti-aciima@pop-os.localdomain>
2023-10-31 09:59:04 -0300

    feat: README.md adicionado, data = db_command() alterado

---
+++ README.md
@@ -0,0 +1,40 @@
+### Sistema de Cadastro 
+
+Um sistema simples, feito com o banco de dados sqlite3 e PySimpleGUI para inserção e ediçao de dados; Nome, CPF, Email e Telefone.
+
+#### Bibliotecas necessarias: 
+
+
+Primeiro crie um ambiente virtual para instalar as dependências
+
+#### Unix
+
+```
+python3 -m venv <venv>
+
+source <venv>/bin/activate
+
+python3 -m pip install pysimplegui
+
+```
+
+#### Windows 
+
+```
+c:\>Python35\python -m venv c:\path\to\myenv
+
+PS C:\> <venv>\Scripts\Activate.ps1
+
+py -m pip install pysimplegui
+```
+
+Tela de cadastro
+
+![Alt text](image.png)
+
+Lista de Usuários
+
+![Alt text](image-1.png)
+
+
+
--- main.py
+++ main.py
@@ -8,10 +8,8 @@ def db_commands(query):
         con.commit()
         return cur.fetchall()
 
-
 layout_title = "Sistema de Cadastro"
 headings = ["Nome","CPF","Email","Telefone"]
-data = db_commands("SELECT * FROM users")
 
 def main_window():
     window_main = [
@@ -19,12 +17,14 @@ def main_window():
     [sg.Text("CPF"),sg.Push(),sg.Input(key='-CPF-')],
     [sg.Text("Email"),sg.Push(),sg.Input(key='-EMAIL-')],
     [sg.Text("telefone"),sg.Push(),sg.Input(key='-PHONE-')],
-    [sg.Button("Cadastrar"),sg.Button("Lista de Usuários",key="users"),sg.Button("Close")]]
+    [sg.Button("Cadastrar"),sg.Button("Lista de Usuários",key="-USERS-"),
+    sg.Button("Sair",key="-CLOSE-")]]
 
     return sg.Window(layout_title,layout=window_main)
 
 def table_window():
-    table = [[sg.Table(values=data, headings=headings,
+    data_table = db_commands("SELECT * FROM users")
+    table = [[sg.Table(values=data_table, headings=headings,
                    auto_size_columns=True,
                    num_rows=10,
                    justification='center',
@@ -37,9 +37,8 @@ def table_window():
 
     return sg.Window(layout_title,layout=table)
 
-
 def layout_update(id):
-    name = db_commands("SELECT name, cpf, email, telefone FROM users WHERE cpf={}".format(id))
+    name = db_commands("SELECT name, cpf, email, phone FROM users WHERE cpf={}".format(id))
     window_edit  = [
         [sg.Text("Nome"),sg.Push(),sg.InputText(name[0][0])],
         [sg.Text("CPF"),sg.Push(),sg.InputText(name[0][1])],
@@ -54,7 +53,7 @@ def main():
     window_1 = main_window()
     while True:
         event, values = window_1.read(timeout=100)
-        if event == sg.WIN_CLOSED or event == "Close":
+        if event == sg.WIN_CLOSED or event == "-CLOSE-":
             window_1.close()
             break
 
@@ -79,7 +78,7 @@ def main():
                         window_1[key]('')
                     sg.popup("Cliente Cadastrado com Sucesso!")
 
-        if event == "users":
+        if event == "-USERS-":
             window_1.Hide()
             window_2 = table_window()
             while True:
@@ -93,6 +92,7 @@ def main():
                     data_event_click = event_table[2][0]
 
                 if event_table == "edit":
+                    data = db_commands("SELECT * FROM users")
                     try:
                         window_3 = layout_update(data[data_event_click][1])
                     except UnboundLocalError:
@@ -101,12 +101,10 @@ def main():
                         while True:
                             event_update, value_update = window_3.read()
                             if event_update == "-SAVE-":
-                                # if data[teste][0] == value_update[0]:
-                                #     sg.popup("Nenhuma alteraçao feita")
                                 db_commands(f"""UPDATE users SET name='{value_update[0]}',
                                         cpf={value_update[1]},
                                         email='{value_update[2]}',
-                                        telefone={value_update[3]}
+                                        phone={value_update[3]}
                                         WHERE cpf={data[data_event_click][1]}""")
                                 
                                 sg.popup("Dados Atualizados")
@@ -118,9 +116,10 @@ def main():
                             
 if __name__=="__main__":
     db_commands("""CREATE TABLE IF NOT EXISTS users(
-                name TEXT,
+                name TEXT NOT NULL,
                 cpf INTEGER NOT NULL UNIQUE PRIMARY KEY,
-                email TEXT UNIQUE,
-                telefone INTEGER)""")
+                email TEXT UNIQUE NOT NULL,
+                phone INTEGER NOT NULL)""")
+    
     main()
 
